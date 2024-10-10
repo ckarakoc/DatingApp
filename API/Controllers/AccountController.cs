8b1a5e9a29f59e39b0ae9bb992db559290722d9d
Celal Karakoc <c.karakoc@live.nl>
2024-10-10 18:43:10 +0200

    Next time on D B Z ...

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")] // account/register
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if (await UserExist(registerDto.Username)) return BadRequest("Username is already taken");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            Username = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Username == loginDto.Username.ToLower());
+        if (user == null) return Unauthorized("Invalid username or password");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return user;
+    }
+
+    private async Task<bool> UserExist(string username)
+    {
+        return await context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower());
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,4 +4,8 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string Username { get; set; }
+    
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
+    
 }
