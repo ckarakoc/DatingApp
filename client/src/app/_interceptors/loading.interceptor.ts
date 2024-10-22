56c53d5336660f25a948209897ee8e02518e96e5
Celal Karakoc <c.karakoc@live.nl>
2024-10-22 13:28:27 +0200

    Finished Course :tada:

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -1,13 +1,14 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from "@angular/core";
 import { BusyService } from "../_services/busy.service";
-import { delay, finalize } from "rxjs";
+import { delay, finalize, identity } from "rxjs";
+import { environment } from "../../environments/environment";
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService);
   busyService.busy();
   return next(req).pipe(
-    delay(1000),
+    (environment.production ? identity : delay(1000)),
     finalize(() => {
       busyService.idle()
     })
